3a07ad79287e506294e48a1c033f529744e9ff1b
pandabrah <javi.den@gmail.com>
2016-08-24 02:28:13 -0700

    added areas that will allow the menu to be grabbed

---
+++ Assets/Scripts/Menu Buttons/GrabInterations.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabInterations : MonoBehaviour {
+
+    private GameObject menuObj;
+    private Color matColor;
+    private float showZone = 200f;
+
+    void Awake()
+    {
+        menuObj = this.transform.parent.parent.gameObject;
+        matColor = this.GetComponent<Material>().color;
+    }
+
+    void OnTriggerEnter()
+    {
+        matColor.a = showZone;
+        menuObj.tag = "Grabbable";
+    }
+
+    void OnTriggerExit()
+    {
+        matColor.a = 0f;
+        menuObj.tag = "Untagged";
+    }
+}
