5f649f9c7862b6657ce941b1f6f24fd2618e6382
pandabrah <javi.den@gmail.com>
2016-08-24 23:21:18 -0700

    Started work on serializing score data

--- Assets/Scripts/Arcade Shooting Scripts/SaveScore.cs
+++ Assets/Scripts/Arcade Shooting Scripts/SaveScore.cs
@@ -1,11 +1,41 @@
 ﻿using UnityEngine;
+using System;
+using System.IO;
 using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
 
-[System.Serializable]
-public class SaveScore : MonoBehaviour{
+public class SaveScore {
 
-    public static SaveScore saveScore;
-    public int[] savedScores;
+    public static void SaveScores()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/scores.sav", FileMode.Create);
 
+        PlayerScores localScores = new PlayerScores();
+
+        formatter.Serialize(stream, localScores);
+        stream.Close();
+    }
+
+    public static void LoadScores()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/scores.sav", FileMode.Open);
+
+        PlayerScores localScores = formatter.Deserialize(stream) as PlayerScores;
+        stream.Close();
+    }
 }
 
+[Serializable]
+public class PlayerScores
+{
+    public int[] scores;
+    public string[] names;
+
+    public PlayerScores()
+    {
+        scores = new int[10];
+        names = new string[10];
+    }
+}
